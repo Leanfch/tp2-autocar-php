807e6903d5ef8e83b2d7da400cac1fb9e2c4ff28
leanfch <leanf94@gmail.com>
2023-04-26 00:02:05 -0300

    add-favicon

--- index.php
+++ index.php
@@ -48,6 +48,7 @@ if (!array_key_exists($vista, $vistasValidas)) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
   <link rel="stylesheet" href="css/style.css">
+  <link rel="shortcut icon" href="assets/autocar-logo.png" type="image/x-icon">
   <title>AutoCar | <?= $titulo ?></title>
 </head>
 
